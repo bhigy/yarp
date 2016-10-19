9ce8c1afe9dd93e869c05bf0347314a6965569a6
Alberto Cardellino <Alberto.Cardellino@iit.it>
2016-10-19 14:24:03 +0200

    Move clock initialization from initMinimum to Network::init

--- src/libYARP_init/src/CustomInit.cpp
+++ src/libYARP_init/src/CustomInit.cpp
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include <yarp/os/Network.h>
+#include <yarp/os/Time.h>
 
 static int __custom_yarp_is_initialized = 0;
 
@@ -34,6 +35,19 @@ extern "C" int __yarp_is_initialized;
 extern "C" void yarpCustomFini() {
 }
 
+void yarpClockInit()
+{
+    yarp::os::ConstString clock = yarp::os::Network::getEnvironment("YARP_CLOCK");
+    if (clock!="") 
+    {
+        yarp::os::Time::useNetworkClock(clock);
+    }
+    else
+    {
+        yarp::os::Time::useSystemClock();
+    }
+}
+
 yarp::os::Network::Network() {
     Network::init();
 }
@@ -45,6 +59,10 @@ yarp::os::Network::~Network() {
 void yarp::os::Network::init() {
     if (__custom_yarp_is_initialized==0) {
         initMinimum();
+        // If we init the clock inside the initMinum, it will loop into itself twice
+        // calling again the initMinimun, ending with __yarp_is_initialized counter 
+        // increased twice.
+        yarpClockInit();
         yarpCustomInit();
     }
     __custom_yarp_is_initialized++;
